7398ce6a6b691114d147c46ac423dd53fdd2eeea
user <411503069@qq.com>
2018-11-17 06:40:46 +0800

    love zhou

---
+++ 基础/89.py
@@ -0,0 +1,17 @@
+a=[1,22,2,]
+print(type(a))
+def fun(a):
+    if a == 1:
+        return 1
+    if a == 2:
+        return 2
+    else:
+        return fun(a-2) + fun(a-1)
+
+for a in range(1,10):
+    if fun(a)<35:
+        if fun(a)%2 == 0:
+            print(fun(a), end=" ")
+    else:
+        print(fun(a), end=" ")
+print(type(fun(a)))
