b38dc0402aaa2f7a5fd1f04580d6a1b4a337aa9f
user <411503069@qq.com>
2018-11-24 09:05:11 +0800

    love zhou

--- 基础/递归函数.py
+++ 基础/递归函数.py
@@ -1,5 +1,5 @@
-a=[1,22,2,]
-print(type(a))
+a=[1,22,2,69999]
+print(type(a))   #打印参数类型
 def fun(a):
     if a == 1:
         return 1
@@ -9,9 +9,16 @@ def fun(a):
         return fun(a-2) + fun(a-1)
 
 for a in range(1,10):
-    if fun(a)<35:
+    if fun(a)<6:
         if fun(a)%2 == 1:
             print(fun(a), end=" ")
     else:
         print(fun(a), end=" ")
+print(type(fun(a)))
+
+for a in range(1,10):
+    if fun(a)<99:
+        if fun(a)%2 == 1:
+            print(fun(a), end=" ")
+
 print(type(fun(a)))
