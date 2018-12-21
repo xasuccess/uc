ee086d4eaac1d58c02a3976ce14fa0a2fd3eb864
user <411503069@qq.com>
2018-12-21 21:31:22 +0800

    love zhou

--- 基础/fun.py
+++ 基础/fun.py
@@ -1,5 +1,30 @@
-#定义一个函数
+# 定义一个函数
+# 只是定义不会执行
+# def关键字，后面跟随一个空格，最后冒号
+# 函数所有代码缩进
 def fun():
-	print("定义一个函数，需要有def开头")
-	
+    print('我是一个函数')
+    print("可以随便定义些么")
+    print("然后可以运行")
+
+
+print("测试下吧")
 fun()
+
+
+def hello(person):
+    print("{0},你好呀".format(person))
+
+
+hello("DL")
+hello("d")
+
+
+def hello(person):
+    print("{0},你怎么啦".format(person))
+    return "我已经和{0}打招呼了，{0}不理睬我".format(person)
+    print("随便打些什么测试下")
+
+
+hello("p")
+print(hello("p"))
